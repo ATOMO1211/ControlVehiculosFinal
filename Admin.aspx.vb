6591ae3079efd3d95d18971e369e054e1a1abd62
karla <karlarios1294@gmail.com>
2025-11-25 20:27:18 -0600

    Semana 10

--- Admin.aspx.vb
+++ Admin.aspx.vb
@@ -1,10 +1,12 @@
-﻿Public Class Home
+﻿Public Class Admin
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Dim Usuario As Usuario = Session("Usuario")
         lblUsuario.Text = "Bienvenido, " & Usuario.NombreUsuario
         lblEmail.Text = "Correo electrónico: " & Usuario.Email
+
+
     End Sub
 
 End Class
--- Login.aspx.vb
+++ Login.aspx.vb
@@ -8,16 +8,20 @@ Public Class Login
     End Sub
 
     Protected Sub btnIniciarSesion_Click(sender As Object, e As EventArgs)
-        Dim Usuario = txtUsuario.Text
-        Dim Password = txtPassword.Text
+        Dim Usuario As String = txtUsuario.Text
+        Dim Password As String = txtPassword.Text
         Dim encrypter As New Simple3Des("MiClaveSecreta123") ' Clave secreta para la encriptación
         Dim pass As String = encrypter.EncryptData(Password)
         Dim dbHelper As New dbLogin()
         Dim isValid As Boolean = dbHelper.ValidateLogin(Usuario, pass)
         If isValid Then
             ' Inicio de sesión exitoso
-            Session("Usuario") = Usuario
-
+            Dim User As Usuario = dbHelper.GetUser(Usuario) ' Obtener el usuario 
+            Session("Usuario") = User ' Almacenar el usuario en la sesión
+            If User.Rol = "2" Then
+                Response.Redirect("Admin.aspx") ' Redirige a la página de administración si es admin
+                Return
+            End If
             Response.Redirect("Home.aspx") ' Redirige a la página principal u otra página segura
         Else
             ' Credenciales incorrectas
